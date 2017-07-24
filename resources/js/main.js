b946712bbe3ba045c03158d9f6a0798529e2af6b
Brent Van Vosselen <brentvanvosselen@Brents-MacBook-Pro.local>
2017-07-24 21:35:58 +0200

    add products to JSON localStorage

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,16 @@
+//THE LOCAL STORAGE OF THE PRODUCTLIST
+var products = (localStorage.getItem('productList')) ? JSON.parse(localStorage.getItem('productList')):{
+    productList: []
+}
+
+//PRODUCT OBJECT
+function product(name,quantity,packagingSize,expirationDate){
+    this.name = name;
+    this.quantity = quantity;
+    this.packagingSize = packagingSize;
+    this.expirationDate = expirationDate;
+}
+
 $(document).ready(function () {
     //INITIALIZATION MATERIALIZE COMPONENTS
 
@@ -8,7 +21,37 @@ $(document).ready(function () {
         selectMonths: true,
         selectYears: 15
     });
+    
+    
+    //ADD PRODUCT
+    $("#addProduct").click(function(){
+        var name = $("#productName").val();
+        var quantity = $("#quantity").val();
+        var packagingSize = $("#packagingSize").val();
+        var expirationDate = $("#expirationDate").val();
+        if(name && quantity && packagingSize && expirationDate){
+            addProduct(name,quantity,packagingSize,expirationDate);
+        }
+    });
 
 
 
 });
+
+
+function addProduct(name, quantity, packagingSize, expirationDate){
+   // var product = new product(name,quantity,packagingSize,expirationDate);
+    var product = new Object();
+    product.name = name;
+    product.quantity = quantity;
+    product.packagingSize = packagingSize;
+    product.expirationDate = expirationDate;
+    products.productList.push(product);
+    updateProducts();
+}
+
+
+//function to update JSON
+function updateProducts(){
+    localStorage.setItem('productList',JSON.stringify(products));
+}
