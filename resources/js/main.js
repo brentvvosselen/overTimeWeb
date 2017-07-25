178fd9862f546c29898a4be64bff985c74dc50f8
Brent Van Vosselen <brentvanvosselen@Brents-MacBook-Pro.local>
2017-07-25 02:09:30 +0200

    add products to html without layout

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,10 +1,10 @@
 //THE LOCAL STORAGE OF THE PRODUCTLIST
-var products = (localStorage.getItem('productList')) ? JSON.parse(localStorage.getItem('productList')):{
+var products = (localStorage.getItem('productList')) ? JSON.parse(localStorage.getItem('productList')) : {
     productList: []
 }
 
 //PRODUCT OBJECT
-function product(name,quantity,packagingSize,expirationDate){
+function Product(name, quantity, packagingSize, expirationDate) {
     this.name = name;
     this.quantity = quantity;
     this.packagingSize = packagingSize;
@@ -21,37 +21,64 @@ $(document).ready(function () {
         selectMonths: true,
         selectYears: 15
     });
-    
-    
+
+
     //ADD PRODUCT
-    $("#addProduct").click(function(){
+    $("#addProduct").click(function () {
         var name = $("#productName").val();
         var quantity = $("#quantity").val();
         var packagingSize = $("#packagingSize").val();
         var expirationDate = $("#expirationDate").val();
-        if(name && quantity && packagingSize && expirationDate){
-            addProduct(name,quantity,packagingSize,expirationDate);
+        if (name && quantity && packagingSize && expirationDate) {
+            addProduct(name, quantity, packagingSize, expirationDate);
         }
     });
 
+    //RENDER PRODUCTS
+    renderProducts();
 
 
 });
 
-
-function addProduct(name, quantity, packagingSize, expirationDate){
-   // var product = new product(name,quantity,packagingSize,expirationDate);
-    var product = new Object();
-    product.name = name;
-    product.quantity = quantity;
-    product.packagingSize = packagingSize;
-    product.expirationDate = expirationDate;
+//function to add a new product to the list
+function addProduct(name, quantity, packagingSize, expirationDate) {
+    var product = new Product(name, quantity, packagingSize, expirationDate);
     products.productList.push(product);
     updateProducts();
+    renderProduct(product);
 }
 
 
 //function to update JSON
-function updateProducts(){
-    localStorage.setItem('productList',JSON.stringify(products));
+function updateProducts() {
+    localStorage.setItem('productList', JSON.stringify(products));
+}
+
+//function to render all the products from JSON
+function renderProducts() {
+    for (var i = 0; i < products.productList.length; i++) {
+        var value = products.productList[i];
+        renderProduct(value);
+    }
+}
+
+//function to render one product to the end of the list
+function renderProduct(product) {
+
+    var item = document.createElement('h3');
+    item.innerHTML = product.name;
+    document.getElementById('productsHTML').appendChild(item);
+
+    /* <div class="col s12 m6 l4">
+                 <div class="card">
+                     <div class="card-image">
+                         <img src="https://www.w3schools.com/w3images/fjords.jpg">
+                         <span class="card-title">Card Title</span>
+                         <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                     </div>
+                     <div class="card-content">
+                         <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                     </div>
+                 </div>
+             </div>*/
 }
